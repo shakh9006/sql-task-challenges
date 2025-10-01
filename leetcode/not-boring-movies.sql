14ab696d33ec620418cc213cfd73086e08b5a15c
swift <waxboz.nematov@gmail.com>
2025-10-01 11:26:20 +0500

    Not Boring Movies

---
+++ leetcode/not-boring-movies.sql
@@ -0,0 +1,8 @@
+-- Source: https://leetcode.com/problems/not-boring-movies/
+
+--Title: Not Boring Movies
+
+SELECT *
+FROM Cinema
+WHERE id % 2 = 1 AND description != 'boring'
+ORDER BY rating DESC;
