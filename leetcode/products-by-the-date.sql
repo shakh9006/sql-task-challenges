daf97d217aa314c326e5ff2c8234486698304f1b
swift <waxboz.nematov@gmail.com>
2025-10-15 11:45:27 +0500

    Group Sold Products By The Date

---
+++ leetcode/products-by-the-date.sql
@@ -0,0 +1,10 @@
+-- Source: Group Sold Products By The Date
+
+--Title: https://leetcode.com/problems/group-sold-products-by-the-date/description/
+
+SELECT
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    STRING_AGG(DISTINCT product, ',' ORDER BY product) AS products
+FROM Activities
+GROUP BY sell_date;
