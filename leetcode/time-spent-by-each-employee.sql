8630bd1acb1e69d8d66bea3292e0f169e127d4d7
swift <waxboz.nematov@gmail.com>
2025-10-22 11:47:03 +0500

    Find Total Time Spent by Each Employee

---
+++ leetcode/time-spent-by-each-employee.sql
@@ -0,0 +1,10 @@
+-- Source: https://leetcode.com/problems/find-total-time-spent-by-each-employee/description/
+
+-- Title: Find Total Time Spent by Each Employee
+
+SELECT
+    event_day AS day,
+    emp_id,
+    SUM(out_time - in_time) AS total_time
+FROM Employees
+GROUP BY 1, 2;
