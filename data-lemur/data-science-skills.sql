a9da7ac1e2653359a45e1f1d6925a7202aa1c431
swift <waxboz.nematov@gmail.com>
2026-04-22 13:02:06 +0500

    Data Science Skills

---
+++ data-lemur/data-science-skills.sql
@@ -0,0 +1,11 @@
+-- Source: https://datalemur.com/questions/matching-skills
+
+--Title: Data Science Skills
+
+SELECT
+  candidate_id
+FROM candidates
+WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(DISTINCT skill) = 3
+ORDER BY candidate_id;
